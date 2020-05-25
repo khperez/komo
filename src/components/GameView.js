cc0cf506ff0240472cdd89851f3d3935278fe61e
Ming Chen Lin <thekenu@gmail.com>
2020-05-24 21:59:16 -0700

    Bind onChangeAnswer to each form

--- src/components/GameView.js
+++ src/components/GameView.js
@@ -3,17 +3,18 @@ import Form from 'react-bootstrap/Form';
 
 export default function GameView({categories, onChange}) {
     const categoryList = categories.map((category) =>
-        <Form.Group>
-            <Form.Label>{category.name}</Form.Label>
-            <Form.Control/>
-        </Form.Group>
+        //<Form onChange={onChange.bind(category.id)}>
+        <Form onChange={onChange.bind(this, category.id)}>
+            <Form.Group>
+                <Form.Label>{category.name}</Form.Label>
+                <Form.Control/>
+            </Form.Group>
+        </Form>
     );
     return (
         <div>
             <h1>Game View</h1>
-            <Form onChange={onChange}>
-                {categoryList}
-            </Form>
+            {categoryList}
         </div>
     )
 }
