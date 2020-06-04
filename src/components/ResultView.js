c68c3fdad8b46fe5f2d31fcd5ba2d7cf96adbf14
Ming Chen Lin <thekenu@gmail.com>
2020-06-03 21:07:04 -0700

    - Add checkbox value to voting - Add submit vote button (that pushes the votes to Firebase)

--- src/components/ResultView.js
+++ src/components/ResultView.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function ResultView({ players }) {
     return (
         <div>
+            <h1>Result View</h1>
             {players}
         </div>
     )
--- src/components/VotingView.js
+++ src/components/VotingView.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-export default function VotingView({ categories, allAnswers, onChange }) {
-
+export default function VotingView({
+    categories, categoryLetter, allAnswers,
+    onChange, voteResults, onSubmitVotes
+}) {
     var checkboxes =
     <div>
         {categories.map(category => {
@@ -11,12 +14,29 @@ export default function VotingView({ categories, allAnswers, onChange }) {
             return  <div key={category.id}>
                         <h2 >{category.name}</h2>
                         {Object.keys(allAnswersForOneCategory).map(uid=> {
-                            const answer = allAnswersForOneCategory[uid].value
-                            return <Form onChange={onChange.bind(this, category.id, uid)} key={category.id + uid}>
-                                       <Form.Group>
-                                           <Form.Check type="checkbox" label={answer}/>
-                                       </Form.Group>
-                                   </Form>
+                            const answer = allAnswersForOneCategory[uid]
+
+                            if (answer.valid) {
+                                return <Form key={category.id + uid}>
+                                        <Form.Group>
+                                            <Form.Check type="checkbox"
+                                                        checked={voteResults[category.id][uid]}
+                                                        onChange={onChange.bind(this, category.id, uid)}
+                                                        label={answer.value}/>
+                                        </Form.Group>
+                                    </Form>
+
+                            } else {
+                                return <Form key={category.id + uid}>
+                                        <Form.Group>
+                                            <Form.Check type="checkbox"
+                                                        checked={voteResults[category.id][uid]}
+                                                        onChange={onChange.bind(this, category.id, uid)}
+                                                        label={answer.value + " (Bad answer, boo!)"}
+                                                        disabled/>
+                                        </Form.Group>
+                                    </Form>
+                            }
                         })}
                     </div>
         })}
@@ -24,8 +44,9 @@ export default function VotingView({ categories, allAnswers, onChange }) {
 
     return (
         <div>
-            <h1>Voting View</h1>
+            <h1>Voting View (Letter: {categoryLetter})</h1>
             {checkboxes}
+            <Button onClick={onSubmitVotes}>Submit Votes</Button>
         </div>
     )
 }
