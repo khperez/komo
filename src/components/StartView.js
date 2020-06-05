8ad3397d7cd5f8376fa33122816133964447b7d6
Katherine Perez <perezkh@gmail.com>
2020-06-05 00:13:47 -0700

    [UI] center start page

--- src/components/StartView.js
+++ src/components/StartView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import GameActions from './GameActions';
 
 const Title = () => (
@@ -6,12 +6,22 @@ const Title = () => (
         Komo
     </div>
 );
+class MadeBy extends Component {
+    render() {
+      return (
+        <div className="Made-by">
+          Made by <a href="https://github.com/thekenu" className="Github-link">kenu</a> & <a href="https://github.com/khperez" className="Github-link">katu</a>
+        </div>
+      )
+    }
+  }
 
 export default function StartView({ onCreate, onJoin }) {
     return (
-        <div>
+        <div className="Start-view">
             <Title />
             <GameActions onCreate={ onCreate } onJoin={ onJoin }/>
+            <MadeBy />
         </div>
     );
 }
