d23faee9fb0f0d0606359ec874b12d7c4c621aa3
Ming Chen Lin <thekenu@gmail.com>
2020-06-14 22:49:55 -0700

    Form validation for host lobby form

--- src/components/HostLobbyView.js
+++ src/components/HostLobbyView.js
@@ -1,28 +1,54 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import Form from "react-bootstrap/Form";
+import { useState } from 'react';
 
 function NumCategoriesForm({changeHandler, submitHandler}) {
+
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+  };
+
+  const min_num_categories = "1";
+  const max_num_categories = "20";
+
+  const min_timer_in_seconds = "1";
+  const max_timer_in_seconds = "999";
+
   return (
       <div className="hostView">
-        <form onSubmit={submitHandler}
-          autoComplete="off">
-          <Form.Group 
-            className="Host-form"
-            onChange={changeHandler}>
+        <Form noValidate validated={validated} onSubmit={handleSubmit} autoComplete="off">
+          <Form.Group className="Host-form">
             <Form.Control
-            placeholder="number of categories"
-            name="numCategories" />
+              required
+              type="number"
+              min={min_num_categories}
+              max={max_num_categories}
+              placeholder={"Number of catergories (" + min_num_categories + " - " + max_num_categories + ")"}
+              name="numCategories"
+              onChange={changeHandler}
+            />
             <Form.Control
-            placeholder="timer in seconds"
-            name="timeRemaining" />
+              required
+              type="number"
+              min={min_timer_in_seconds}
+              max={max_timer_in_seconds}
+              placeholder={"timer in seconds (" + min_timer_in_seconds + " - " + max_timer_in_seconds + ")"}
+              name="timeRemaining"
+              onChange={changeHandler}
+            />
           </Form.Group>
           <div className="Host-settings-submit">
-            <Button type="submit" label="login" variant="contained">
-              Start Game
-            </Button>
+            <Button type="submit" variant="contained">Start Game</Button>
           </div>
-        </form>
+        </Form>
       </div>
   );
 }
