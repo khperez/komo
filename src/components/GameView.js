6cb579819957cafc24193e0648134c93e7cdedc7
Katherine Perez <perezkh@gmail.com>
2020-06-17 20:04:17 -0700

    update GameView styling

--- src/components/GameView.js
+++ src/components/GameView.js
@@ -2,20 +2,22 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 
 export default function GameView({categories, onChange, onSubmit}) {
-  const categoryList = categories.map((category) =>
+  const categoryList = categories.map((category, index) =>
     <Form
       onChange={onChange.bind(this, category.id)}
       key={category.id}
       onSubmit={onSubmit}>
       <Form.Group>
-        <Form.Label>{category.name}</Form.Label>
-        <Form.Control/>
+        <Form.Label className="Game-form-label">
+          <span className="Game-form-input-index">{index+1}</span> {category.name}
+         </Form.Label>
+        <Form.Control className="Game-form-input-answer"/>
       </Form.Group>
     </Form>
   );
   return (
-    <div className="Game-form">
-      <div>
+    <div className="Game-form-container">
+      <div className="Game-form-input">
         {categoryList}
       </div>
     </div>
