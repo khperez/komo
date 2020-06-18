41ad832ad1786a734efe9853e9c7427bb1664415
Katherine Perez <perezkh@gmail.com>
2020-06-17 19:28:41 -0700

    [UI] add more spacing in host form

--- src/components/HostLobbyView.js
+++ src/components/HostLobbyView.js
@@ -28,24 +28,30 @@ export default function HostLobbyView(props) {
       <div className="hostView" onSubmit={props.onSubmit}>
         <Form noValidate validated={validated} onSubmit={handleSubmit} autoComplete="off">
           <Form.Group className="Host-form">
-            <Form.Control
-              required
-              type="number"
-              min={min_num_categories}
-              max={max_num_categories}
-              placeholder={"Number of categories (" + min_num_categories + " - " + max_num_categories + ")"}
-              name="numCategories"
-              onChange={props.onChange}
-            />
-            <Form.Control
-              required
-              type="number"
-              min={min_timer_in_seconds}
-              max={max_timer_in_seconds}
-              placeholder={"timer in seconds (" + min_timer_in_seconds + " - " + max_timer_in_seconds + ")"}
-              name="timeRemaining"
-              onChange={props.onChange}
-            />
+            <div className="Host-form-category-container">
+              <Form.Label className="Host-form-category">Number of categories</Form.Label>
+              <Form.Control
+                required
+                type="number"
+                min={min_num_categories}
+                max={max_num_categories}
+                placeholder={min_num_categories + " - " + max_num_categories}
+                name="numCategories"
+                onChange={props.onChange}
+              />
+            </div>
+            <div>
+              <Form.Label className="Host-form-category">Timer (seconds)</Form.Label>
+              <Form.Control
+                required
+                type="number"
+                min={min_timer_in_seconds}
+                max={max_timer_in_seconds}
+                placeholder={min_timer_in_seconds + " - " + max_timer_in_seconds}
+                name="timeRemaining"
+                onChange={props.onChange}
+              />
+            </div>
           </Form.Group>
           <div className="Host-settings-submit">
             <Button type="submit" variant="contained">Start Game</Button>
