558f9253156cda90de811f07b7bbd1e1f8b3959d
Katherine Perez <perezkh@gmail.com>
2020-06-17 19:02:36 -0700

    remove console logging for production builds

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+if (process.env.NODE_ENV === 'production') {
+  console.log = function () {}
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
